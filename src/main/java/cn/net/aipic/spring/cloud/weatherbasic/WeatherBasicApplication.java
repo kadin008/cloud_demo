1f43e1097d2c7f07f71f0bab66318a69d3977270
kdin008 <wangchengbin_2009@vip.163.com>
2018-06-20 10:48:38 +0800

    新项目

---
+++ src/main/java/cn/net/aipic/spring/cloud/weatherbasic/WeatherBasicApplication.java
@@ -0,0 +1,12 @@
+package cn.net.aipic.spring.cloud.weatherbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherBasicApplication.class, args);
+    }
+}
